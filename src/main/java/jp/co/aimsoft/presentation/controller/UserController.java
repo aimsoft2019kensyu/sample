dcdda3abbbb06242aea7d9ce4ef05260d85c4f1d
mineArrow <t.iwai.git@gmail.com>
2019-05-24 02:22:07 +0900

    test

--- src/main/java/jp/co/aimsoft/presentation/controller/UserController.java
+++ src/main/java/jp/co/aimsoft/presentation/controller/UserController.java
@@ -2,7 +2,6 @@ package jp.co.aimsoft.presentation.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +16,6 @@ import jp.co.aimsoft.presentation.form.UserForm;
 @Controller
 public class UserController {
 
-	@Autowired
 	private UserService service;
 
 	@RequestMapping(value = "/user/registration/show", method = RequestMethod.GET)
